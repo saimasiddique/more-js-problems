b3a98454104cea37f01b0ca8530a3e6a7b4a04da
saimasiddique <siddiquesaima16022@gmail.com>
2022-02-03 13:08:21 +0600

    first commit

---
+++ input-error.js
@@ -0,0 +1,19 @@
+function add(num1, num2) {
+    const sum = num1 + num2;
+
+    return sum;
+
+}
+
+const firstTotal = add(63, 7);
+//console.log(firstTotal);
+const secondTotal = add(53, 981);
+//console.log(secondTotal);
+
+function multiply(num1, num2) {
+    const result = num1 * num2;
+    return result;
+}
+
+const firstResult = multiply(13, 12);
+console.log(firstResult);
